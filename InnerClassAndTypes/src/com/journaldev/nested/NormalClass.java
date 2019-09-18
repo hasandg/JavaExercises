94d50da5c3476837172bff338a1a02dae3bfe25c
hasandg <hasan.dg@hotmail.com>
2019-09-18 10:35:35 +0300

    Message

---
+++ InnerClassAndTypes/src/com/journaldev/nested/NormalClass.java
@@ -0,0 +1,14 @@
+package com.journaldev.nested;
+
+/**
+ * Created by : hdag
+ * Date: 16.09.2019
+ * Time: 11:00
+ */
+public class NormalClass {
+
+
+    public static void foo(){
+        System.out.println("NormalClass.foo() called!");
+    }
+}
